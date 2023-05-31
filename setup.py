3bdbeff0e81e54cb47dc0625702fb834af3b6d79
I-HaoChen <i-hao.chen@live.de>
2023-05-31 15:27:30 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Food-Anticipatory-Behaviour',
+    version='1.0.0',
+    packages=['src', 'src.fish_telemetry_faa', 'src.fish_telemetry_faa.utils', 'src.fish_telemetry_faa.clustering',
+              'src.fish_telemetry_faa.statistics'],
+    url='',
+    license='Apache 2.0 license',
+    author='I-Hao Chen',
+    author_email='chen@norceresearch.no',
+    description='Software for: Food Anticipatory Behaviour on European Seabass in Sea Cages: Activity-, Positioning- and Density-based approaches'
+)
